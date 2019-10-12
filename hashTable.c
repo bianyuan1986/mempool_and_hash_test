aa4579fe760770e27003779aabeb2fba0ef49173
bianyuan1986 <231881831@qq.com>
2019-10-12 19:41:35 +0800

    update hash table

--- hashTable.c
+++ hashTable.c
@@ -32,21 +32,43 @@ struct HashTableStatis
 	unsigned int totalMem;
 };
 
+typedef int (*fpInsert)(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout);
+typedef void* (*fpSearch)(struct hashTable *htbl, void *data, int dLen, void *key, void *copy, struct UpdateCallBack *callback);
+
+struct HashInterface
+{
+	fpInsert insert;
+	fpSearch search;
+};
+
 struct hashTable
 {
 	struct ListElem *bucket;
 	unsigned int bucketSize;
 	unsigned int capacity;
 
+	enum HASH_STRATEGY mode;
 	int probeStep;
 	int factor;
+
+	struct HashInterface *inf;
 	struct HashTableOps ops;
 	struct HashTableStatis st;
 };
 
+static void *HashDefaultMalloc(size_t size)
+{
+	return malloc(size);
+}
+
+static void HashDefaultFree(void *ptr, int len)
+{
+	free(ptr);
+}
+
 static int Init(struct hashTable *htbl, int size)
 {
-	unsigned int i = 0;
+	int i = 0;
 
 	htbl->capacity = size;
 	htbl->bucketSize = htbl->factor*size;
@@ -66,7 +88,7 @@ static int Init(struct hashTable *htbl, int size)
 	return 0;
 }
 
-static int InsertElem(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout)
+static int InsertElemSelfExpired(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout)
 {
 	int idx = 0;
 	struct ListElem *elem = NULL;
@@ -75,8 +97,7 @@ static int InsertElem(struct hashTable *htbl, void *data, int dLen, void *key, v
 	int ret = RET_FAILED;
 	uint64_t expired = 0;
 
-	currentTime = rte_rdtsc();
-	expired = currentTime + timeout;
+	expired = timeout;
 	idx = htbl->ops.hash(data, dLen, key)%htbl->bucketSize;
 	elem = &htbl->bucket[idx];
 
@@ -122,17 +143,19 @@ FAILED:
 	return ret;
 }
 
-static void *FindElem(struct hashTable *htbl, void *data, int dLen, void *key, void *value)
+static void *FindElemSelfExpired(struct hashTable *htbl, void *data, int dLen, void *key, void *copy, struct UpdateCallBack *callback)
 {
 	int idx = 0;
 	struct ListElem *elem = NULL;
 	int count = 0;
 	int find = 0;
 	uint64_t currentTime = 0;
+	struct HashNodeCopy *cp = NULL;
 
 	idx = htbl->ops.hash(data, dLen, key)%htbl->bucketSize;
 	elem = &htbl->bucket[idx];
 	currentTime = rte_rdtsc();
+	cp = (struct HashNodeCopy*)copy;
 
 	while( count <= htbl->probeStep )
 	{
@@ -142,9 +165,10 @@ static void *FindElem(struct hashTable *htbl, void *data, int dLen, void *key, v
 				(currentTime < elem->timeout) )
 		{
 			find = 1;
-			if( value )
+			if( cp && cp->value )
 			{
-				htbl->ops.assignValue(elem->value, value);
+				htbl->ops.assignValue(elem->value, cp->value);
+				cp->expired = elem->timeout;
 			}
 			rte_rwlock_read_unlock(&elem->rwlock);
 			break;
@@ -155,10 +179,136 @@ static void *FindElem(struct hashTable *htbl, void *data, int dLen, void *key, v
 		count++;
 	}
 
+	if( callback && callback->update && find )
+	{
+		rte_rwlock_write_lock(&elem->rwlock);
+		callback->update(elem->value, callback->userData);	
+		rte_rwlock_write_unlock(&elem->rwlock);
+	}
 	return (find?elem:NULL);
 }
 
-void *hash_table_find(struct hashTable *htbl, void *data, int dLen, void *key, void *value)
+static int InsertElemLRU(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout)
+{
+	int idx = 0;
+	struct ListElem *elem = NULL;
+	uint64_t currentTime = 0;
+	int count = 0;
+	int ret = RET_FAILED;
+	struct ListElem *oldest = NULL;
+
+	idx = htbl->ops.hash(data, dLen, key)%htbl->bucketSize;
+	elem = &htbl->bucket[idx];
+	oldest = elem;
+
+	while( count < htbl->probeStep )
+	{
+		rte_rwlock_write_lock(&elem->rwlock);
+		if( elem->status == STATUS_AVAILABLE )
+		{
+			elem->key = key;
+			elem->value = value;
+			elem->timeout = timeout;
+			elem->status = STATUS_USE;
+			ret = RET_NEW;
+			rte_rwlock_write_unlock(&elem->rwlock);
+			break;
+		}
+
+		if( (elem->status == STATUS_USE) && htbl->ops.cmp(elem->key, key) )
+		{
+			htbl->ops.assignKey(key, elem->key);
+			htbl->ops.assignValue(value, elem->value);
+			elem->timeout = timeout;
+			ret = RET_OCCUPY;
+			rte_rwlock_write_unlock(&elem->rwlock);
+			break;
+		}
+
+		if( elem->timeout < oldest->timeout )
+		{
+			oldest = elem;
+		}
+		rte_rwlock_write_unlock(&elem->rwlock);
+
+		elem++;
+		count++;
+		rte_atomic32_inc(&htbl->st.collision);
+	}
+
+	if( ret == RET_FAILED )
+	{
+		rte_rwlock_write_lock(&oldest->rwlock);
+		htbl->ops.assignKey(key, oldest->key);
+		htbl->ops.assignValue(value, oldest->value);
+		rte_rwlock_write_unlock(&oldest->rwlock);
+		ret = RET_OCCUPY;
+	}
+
+	return ret;
+}
+
+static void *FindElemLRU(struct hashTable *htbl, void *data, int dLen, void *key, void *copy, struct UpdateCallBack *callback)
+{
+	int idx = 0;
+	struct ListElem *elem = NULL;
+	int count = 0;
+	int find = 0;
+	uint64_t currentTime = 0;
+	struct HashNodeCopy *cp = NULL;
+
+	idx = htbl->ops.hash(data, dLen, key)%htbl->bucketSize;
+	elem = &htbl->bucket[idx];
+	currentTime = rte_rdtsc();
+	cp = (struct HashNodeCopy*)copy;
+
+	while( count <= htbl->probeStep )
+	{
+		rte_rwlock_read_lock(&elem->rwlock);
+		if( (elem->status == STATUS_USE) && 
+				(htbl->ops.cmp(elem->key, key) == 1) )
+		{
+			find = 1;
+			if( cp && cp->value )
+			{
+				htbl->ops.assignValue(elem->value, cp->value);
+				cp->expired = elem->timeout;
+			}
+			elem->timeout = currentTime;
+			rte_rwlock_read_unlock(&elem->rwlock);
+			break;
+		}
+		rte_rwlock_read_unlock(&elem->rwlock);
+
+		elem++;
+		count++;
+	}
+
+	if( callback && callback->update && find )
+	{
+		rte_rwlock_write_lock(&elem->rwlock);
+		callback->update(elem->value, callback->userData);	
+		rte_rwlock_write_unlock(&elem->rwlock);
+	}
+	return (find?elem:NULL);
+}
+
+struct HashInterface gHashInf[HASH_STRATEGY_MAX] =
+{
+	[HASH_STRATEGY_SELF_EXPIRED] = 
+	{
+		.insert = InsertElemSelfExpired,
+		.search = FindElemSelfExpired
+	},
+
+	[HASH_STRATEGY_LRU] = 
+	{
+		.insert = InsertElemLRU,
+		.search = FindElemLRU
+	}
+};
+
+void *hash_table_find(struct hashTable *htbl, void *data, int dLen, void *key, void *copy, struct UpdateCallBack *callback)
 {
 	struct ListElem *elem = NULL;
 	if( !htbl || !key || !dLen )
@@ -166,8 +316,20 @@ void *hash_table_find(struct hashTable *htbl, void *data, int dLen, void *key, v
 		return NULL;
 	}
 
-	elem = FindElem(htbl, data, dLen, key, value);
+	elem = htbl->inf->search(htbl, data, dLen, key, copy, callback);
 	return elem?elem->value:NULL;
+}
+
+int hash_table_update(struct hashTable *htbl, void *data, int dLen, void *key, struct UpdateCallBack *callback)
+{
+	struct ListElem *elem = NULL;
+	if( !htbl || !key || !dLen )
+	{
+		return -1;
+	}
+
+	elem = htbl->inf->search(htbl, data, dLen, key, NULL, callback);
+	return elem?0:-1;
 }
 
 int hash_table_insert(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout)
@@ -177,10 +339,10 @@ int hash_table_insert(struct hashTable *htbl, void *data, int dLen, void *key, v
 		return -1;
 	}
 
-	return InsertElem(htbl, data, dLen, key, value, timeout);
+	return htbl->inf->insert(htbl, data, dLen, key, value, timeout);
 }
 
-struct hashTable *hash_table_create(unsigned int capacity, struct HashTableOps *ops)
+struct hashTable *hash_table_create(unsigned int capacity, enum HASH_STRATEGY mode, struct HashTableOps *ops)
 {
 	struct hashTable *htbl = NULL;
 	int ret = 0;
@@ -190,14 +352,19 @@ struct hashTable *hash_table_create(unsigned int capacity, struct HashTableOps *
 		printf("hash/cmp/assignKey/assignValue interface must be provided!\n");
 		goto FAILED;
 	}
+	if( mode >= HASH_STRATEGY_MAX )
+	{
+		printf("Unsupported mode!\n");
+		goto FAILED;
+	}
 
 	if( !ops->mallocFunc )
 	{
-		ops->mallocFunc = malloc;
+		ops->mallocFunc = HashDefaultMalloc;
 	}
 	if( !ops->freeFunc )
 	{
-		ops->freeFunc = free;
+		ops->freeFunc = HashDefaultFree;
 	}
 	htbl = ops->mallocFunc(sizeof(struct hashTable));
 
@@ -208,7 +375,9 @@ struct hashTable *hash_table_create(unsigned int capacity, struct HashTableOps *
 	}
 	htbl->factor = 3;
 	htbl->probeStep = PROBE_COUNT;
+	htbl->mode = mode;
 	htbl->ops = *ops;
+	htbl->inf = &gHashInf[mode];
 
 	ret = Init(htbl, capacity);
 	if( ret < 0 )
@@ -234,8 +403,8 @@ void hash_table_destroy(struct hashTable *htbl)
 		return;
 	}
 
-    htbl->ops.freeFunc(htbl->bucket);	
-	htbl->ops.freeFunc(htbl);
+    htbl->ops.freeFunc(htbl->bucket, sizeof(struct ListElem)*htbl->bucketSize);	
+	htbl->ops.freeFunc(htbl, sizeof(*htbl));
 }
 
 void hash_table_assess(struct hashTable *htbl)
--- hashTable.h
+++ hashTable.h
@@ -14,6 +14,9 @@
 #ifndef _HASHTABLE_H_
 #define _HASHTABLE_H_
 
+#include <stddef.h>
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -22,18 +25,33 @@ extern "C" {
 #define RET_NEW 0
 #define RET_OCCUPY 1
 
+enum HASH_STRATEGY
+{
+	HASH_STRATEGY_SELF_EXPIRED,
+	HASH_STRATEGY_LRU,
+	HASH_STRATEGY_MAX
+};
+
 struct hashTable;
 /*check weather two keys are identical*/
 typedef int (*fpCompare)(void *key1, void *key2);
 /*calculate the hash info of data and stored in key*/
 typedef int (*fpHash)(void *data, int dLen, void *key);
 typedef void* (*fpMalloc)(size_t size);
-typedef void (*fpFree)(void *ptr);
+typedef void (*fpFree)(void *ptr, int len);
 typedef void (*fpAssess)(void *data);
 /*copy the content of key from src to dst*/
 typedef void (*fpAssignK)(void *src, void *dst);
 /*copy the content of value from src to dst*/
 typedef void (*fpAssignV)(void *src, void *dst);
+/*update the node content when find a node in hash table*/
+typedef void (*fpUpdateV)(void *v, void *userData);
+
+struct UpdateCallBack
+{
+	fpUpdateV update;
+	void *userData;
+};
 
 struct HashTableOps
 {
@@ -46,17 +64,24 @@ struct HashTableOps
 	fpAssess assessFunc;
 };
 
+struct HashNodeCopy
+{
+	uint64_t expired;
+	void *value;
+};
+
 /*
  * @Create hash table and init
  *
  * @param
  *  capacity: the maximum count number that can be stored in the hash table
+ *  mode: specify different expiration-strategy
  *  ops: user-defined interface 
  *
  * @return
  *  The hash table create by this function
  */
-struct hashTable *hash_table_create(unsigned int capacity, struct HashTableOps *ops);
+struct hashTable *hash_table_create(unsigned int capacity, enum HASH_STRATEGY mode, struct HashTableOps *ops);
 /*
  * @Insert node into hash table
  *
@@ -75,20 +100,38 @@ struct hashTable *hash_table_create(unsigned int capacity, struct HashTableOps *
  */
 int hash_table_insert(struct hashTable *htbl, void *data, int dLen, void *key, void *value, uint64_t timeout);
 /*
- * @search node in hash table
+ * @search node in hash table and maybe update the node 
  *
  * @param
  *  htbl: hash table to be searched for
  *  data: the data used for calculating hash value
  *  dLen: data length
  *  key: calculate the hash info of data and stored in key struct which is used for searching in hash table
- *  value: a copy of the value of the target node.
+ *  copy: a copy of the value of the target node and the time when the node expire.
+ *  callback: update the value when find the node in hash table. The operation is protected by write-lock
  *
  * @return
  *  NULL: not find
  *  Not NULL: the pointer to the node
  */
-void *hash_table_find(struct hashTable *htbl, void *data, int dLen, void *key, void *value);
+void *hash_table_find(struct hashTable *htbl, void *data, int dLen, void *key, void *copy, struct UpdateCallBack *callback);
+
+/*
+ * @update node in hash table
+ *
+ * @param
+ *  htbl: hash table to be searched for
+ *  data: the data used for calculating hash value
+ *  dLen: data length
+ *  key: calculate the hash info of data and stored in key struct which is used for searching in hash table
+ *  callback: update the value when find the node in hash table. 
+ *
+ * @return
+ *  0: success 
+ *  -1: failed 
+ */
+int hash_table_update(struct hashTable *htbl, void *data, int dLen, void *key, struct UpdateCallBack *callback);
+
 void hash_table_destroy(struct hashTable *htbl);
 void hash_table_assess(struct hashTable *htbl);
 
